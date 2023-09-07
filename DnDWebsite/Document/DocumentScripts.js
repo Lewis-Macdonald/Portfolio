f1c009cc7cea8e321722c970c0ac798407a8838a
Lewis-Macdonald <144017981+Lewis-Macdonald@users.noreply.github.com>
2023-09-07 14:43:26 +0100

    Update DocumentScripts.js; added var to doctitles and doctxt, closed eventlistener properly.

--- DnDWebsite/Document/DocumentScripts.js
+++ DnDWebsite/Document/DocumentScripts.js
@@ -1,5 +1,5 @@
-newdoctitle = "";
-newdoctxt = '';
+var newdoctitle = "";
+var newdoctxt = '';
 
 addEventListener("load", (event) => {
   let el = document.body;
@@ -11,15 +11,15 @@ addEventListener("load", (event) => {
   //console.log(el.innerHTML);
   newdoctitle = divplace.getAttribute('data-doctitle');
   newdoctxt = BaseUrl + divplace.getAttribute('data-doctxt');
-  ReadFile("ScrollTemplate.html", function(TemplateContent)
-  {
+  ReadFile("ScrollTemplate.html", function(TemplateContent){
     content = TemplateContent;
     let scripts = scriptplace.innerHTML;
     //console.log(scripts);
     content += scripts;
     //console.log(content);
     el.innerHTML = content;
-});
+  });
+})
 
 function InsertHTML(doctitle, doctxt) {
   ReadFile("GenericLetter.html", function(Information) {
